e4218289926787571ea65814f96cacd6db4fd803
mihaitaran <mihaiitarann@gmail.com>
2022-01-13 18:17:53 +0100

    exercicio 4

---
+++ mapreduce/exercicio1_4/reducer_4.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+import sys
+
+oldKey = None
+TopSale=0
+
+# Loop around the data
+# It will be in the format key\tval
+# Where key is the store name, val is the sale amount
+#
+# All the sales for a particular store will be presented,
+# then the key will change and we'll be dealing with the next store
+
+for line in sys.stdin:
+    data_mapped = line.strip().split("\t")
+    if len(data_mapped) != 2:
+        # Something has gone wrong. Skip this line.
+        continue
+
+    thisKey, thisSale = data_mapped
+
+    # Escribe un par key:value ante un cambio na key
+    # Reinicia o total   
+    if float(thisSale) > TopSale:
+         TopSale = float(thisSale)
+
+
+
+print('O maximo absoluto é ' ,TopSale)
